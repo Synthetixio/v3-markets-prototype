20b94b0530c77f7dd9f82d88453853af91509d20
Noah Litvin <noah.litvin@gmail.com>
2023-05-05 11:15:30 -0400

    x

--- src/pages/spot/SpotMarket.tsx
+++ src/pages/spot/SpotMarket.tsx
@@ -19,48 +19,46 @@ export function SpotMarket() {
 
   return (
     <Flex height="100vh" maxHeight="100vh" flexDirection="column">
-      <>
-        <Header isSpot />
-        <Flex flex="1" minHeight="0" overflow="hidden">
-          <Flex
-            borderRight="1px solid rgba(255,255,255,0.2)"
-            flex="2"
-            height="100%"
-            flexDirection="column"
+      <Header isSpot />
+      <Flex flex="1" minHeight="0" overflow="hidden">
+        <Flex
+          borderRight="1px solid rgba(255,255,255,0.2)"
+          flex="2"
+          height="100%"
+          flexDirection="column"
+        >
+          <Box
+            display="block"
+            borderBottom="1px solid rgba(255,255,255,0.2)"
+            p="4"
           >
-            <Box
-              display="block"
-              borderBottom="1px solid rgba(255,255,255,0.2)"
-              p="4"
-            >
-              <Heading display="inline-block" size="md">
-                snxETH Spot Market
-              </Heading>
-              <Text opacity="0.5" display="inline-block" ml="1.5">
-                (ID {id})
-              </Text>
-              <Box float="right">
-                <ArrowUpDownIcon />
-              </Box>
+            <Heading display="inline-block" size="md">
+              snxETH Spot Market
+            </Heading>
+            <Text opacity="0.5" display="inline-block" ml="1.5">
+              (ID {id})
+            </Text>
+            <Box float="right">
+              <ArrowUpDownIcon />
             </Box>
-            <SpotMarketForm id={id} />
+          </Box>
+          <SpotMarketForm id={id} />
 
-            <Box flex="1" overflowY="auto" p="4">
-              <MarketDetails id={id} />
-            </Box>
-          </Flex>
-
-          <Box flex="5" pb="32px">
-            <Flex direction="column" height="100%" width="100%">
-              <AdvancedRealTimeChart
-                theme="dark"
-                autosize
-                symbol={"PYTH:ETHUSD"}
-              ></AdvancedRealTimeChart>
-            </Flex>
+          <Box flex="1" overflowY="auto" p="4">
+            <MarketDetails id={id} />
           </Box>
         </Flex>
-      </>
+
+        <Box flex="5" pb="32px">
+          <Flex direction="column" height="100%" width="100%">
+            <AdvancedRealTimeChart
+              theme="dark"
+              autosize
+              symbol={"PYTH:ETHUSD"}
+            ></AdvancedRealTimeChart>
+          </Flex>
+        </Box>
+      </Flex>
     </Flex>
   );
 }
