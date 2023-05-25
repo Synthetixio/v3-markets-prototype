17f1a9432333404b763da5c5d927c2b618fae8a8
Noah Litvin <noah.litvin@gmail.com>
2023-05-25 15:44:05 -0400

    revert

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -24,10 +24,7 @@ export const useContract = (name: ContractName) => {
   const provider = useProvider();
   const { data: signer } = useSigner();
 
-  const fallbackNetwork = name.startsWith("SPOT_")
-    ? "optimism"
-    : "optimism-goeri";
-  const contract = contracts[chain?.network || fallbackNetwork][name];
+  const contract = contracts[chain?.network || "optimism"][name];
 
   if (!contract) {
     throw new Error(`Contract "${name}" not found on network "${NETWORK}"`);
